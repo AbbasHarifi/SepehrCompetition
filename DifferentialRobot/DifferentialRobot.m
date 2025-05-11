c3b9d008db7f2d676c8b8b3f18d2c95f23982c51
AbbasHarifi <harifi@hormozgan.ac.ir>
2025-05-11 20:54:25 +0330

    update

--- DifferentialRobot/DifferentialRobot.m
+++ DifferentialRobot/DifferentialRobot.m
@@ -1,64 +1,21 @@
 close all
 clc
-Robot=CreatRobot(0,0,0,0.02,.1);
-ShowRobot(Robot)
-figure
-xf=-.40;
-yf=-.30;
-thetaf=pi/6;
-
-tfinal=20;
+tfinal=1;
 dt=0.01;
 T=0:dt:tfinal;
-
-X=[x];
-Y=[y];
-Theta=[theta];
-
-kp1=250;
-kp2=50;
-
+Robot=CreateRobot(0,0,0,0.02,.1,dt,20);
+Target=struct('x',0.8,'y',0.7);
+Environment=struct('xmin',-1.5,'xmax',1.5,'ymin',-1.5,'ymax',1.5);
 for t=T
-    if mod(t,1)==0
-        x0=x;
-        y0=y;
-        theta0=theta;
-        xf=rand-.5;
-        yf=rand-.5;
-        X=[x];
-        Y=[y];
-        Theta=[theta];
-    end
-    d=sqrt((x-xf)^2+(y-yf)^2);
-    alpha=atan2(yf-y,xf-x);
-    error=alpha-theta;
-    wr=kp1*d*exp(-error^2)+kp2*error;
-    wl=kp1*d*exp(-error^2)-kp2*error;
-
-    x=x+dt*(wr+wl)*r*cos(theta)/2;
-    y=y+dt*(wr+wl)*r*sin(theta)/2;
-    theta=mod(theta+dt*r*(wr-wl)/b,2*pi);
-    if theta>pi
-        theta=theta-2*pi;
-    end
-    X(end+1)=x;
-    Y(end+1)=y;
-    Theta(end+1)=theta;
-    plot(X,Y,xf,yf,'ro','LineWidth',2)
+    [wl,wr]=Controller(Robot,Target);
+    Robot=UpdateRobot(Robot,wl,wr);
     ShowRobot(Robot)    
-    axis equal
-    axis([-.50 .50 -.50 .50])
+    ShowEnvironment(Environment,Target)
+    writeAnimation(gcf,'loop.gif','LoopCount',1)
     pause(dt)
 end
 
-function Robot=CreatRobot(x,y,theta,r,b)
-Robot=struct('x',x,'y',y,'theta',theta,'r',r,'b',b);
-end
 
-function ShowRobot(Robot)
-t=0:.1:2*pi;
-x=Robot.x+Robot.r*cos(t);
-y=Robot.y+Robot.r*sin(t);
-fill(x,y,'red')
-end
+
+
     
